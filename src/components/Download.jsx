fdc2a3def51c32aff004c0f9436efa8ac5632478
Elena WELTY <157579304+Ellen010@users.noreply.github.com>
2026-03-12 17:23:49 +0100

    Update Download.jsx

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
+
+const Download = () => {
+    return (
+        <div>
+            <h2 className="text-center text-6xl">
+                Download
+            </h2>
+        </div>
+    )
+}
+export default Download
 
