69a0f42f045e826f7f83b668b8d481fd55023630
Ellen010 <elenawelty@gmail.com>
2026-03-16 12:39:09 +0100

    Hero spacing modifications

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <section>
+        <section className="my-30">
             <div>
-                <p className="text-2xl">
+                <div className="m-9 relative max-w-6xl mx-auto leading-relaxed place-items-baseline space-y-4 text-2xl">
                     Tired of the tedious, time-consuming process of tracking your cannabis plants for METRC compliance? Say goodbye to manual data entry and hello to Easy-uana — the revolutionary iOS app designed exclusively for cannabis growers. With Easy-uana, you can effortlessly capture RFID tag numbers using just your iPhone, instantly format them into METRC-compliant templates, and download them straight to your computer. Simplifying compliance has never been this easy, affordable, or efficient.
-                </p>
+                </div>
 
             </div>
         </section>
