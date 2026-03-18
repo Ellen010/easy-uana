4afa7c35458fd23899f5b81db55761f7b5ea5c45
Ellen010 <elenawelty@gmail.com>
2026-03-18 11:32:00 +0100

    Contact form final corrections, button edits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="bg-stone-300 my-1 h-[3px] w-3/4 hover:bg-green-600 transition-colors duration-300"></div>
             <div>
                 <p className="text-sm text-center">
-                    Copyright @ 2026 3Degrees Inc. All rights reserved.
+                    Copyright @ 2026 Yvelines Technologies Inc. All rights reserved.
                 </p>
             </div>
         </section>
